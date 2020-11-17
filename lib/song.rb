5a057fb4a7604711b57147a2f85f70c7e8922382
Your Username <you@example.com>
2020-11-17 00:01:04 -0500

    commit tree 31fda8b1a0c0095961c0417468c59d8f64639351

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Song 
+attr_accessor :name, :artist
+
+@@all = []
+
+def initialize(name) 
+    @name = name 
+    @@all << self 
+end  
+
+def artist=(artist) 
+    @artist = artist 
+end 
+
+def artist 
+    @artist
+end 
+
+def self.all 
+    @@all 
+end 
+
+def artist_name
+  if self.artist
+    self.artist.name 
+  end 
+end 
+end 
